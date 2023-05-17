a3fe9c3c9891d8d94d70b51be9fcb81b549f19c5
kandfg <kandfg1@gmail.com>
2023-05-17 15:48:30 +0800

    add the openweather

--- line_weather/line-weather/line-weather.ts
+++ line_weather/line-weather/line-weather.ts
@@ -1,7 +1,8 @@
 import { Client, validateSignature, exceptions } from '@line/bot-sdk';
-
+import fetch from "node-fetch";
 const handler = async (event: any) => {
   // 取得環境變數
+  console.log(event); // 添加此行日志输出
   const clientConfig = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
     channelSecret: process.env.CHANNEL_SECRET || '',
@@ -12,7 +13,7 @@ const handler = async (event: any) => {
   // 為 Webhook 驗證做準備
   const signature = event.headers['x-line-signature'];
   const body = event.body;
-
+  const WEAKey=process.env.WEA_TOKEN;
   // Line Bot 運作邏輯
   const handleEvent = async (event: any) => {
     if (event.type !== 'message' || event.message.type !== 'text') {
@@ -20,13 +21,48 @@ const handler = async (event: any) => {
     }
     const { replyToken } = event;
     const { text } = event.message;
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${text}&limit=5&appid=${WEAKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // 在这里处理获取到的数据
+      console.log(data);
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEAKey}&units=Metric`);
+      // 在这里进行后续的处理
+      // ...
+    })
+    .then((response) => response.json())
+    .then((data) => {
+    // 处理第二个fetch请求返回的数据
+      console.log(data);
 
-    // Create a new message.
-    const response = {
-      type: 'text',
-      text: `而 Netlify 的回音盪漾著：${text}～`,
-    };
-    await client.replyMessage(replyToken, response);
+      const city= data.name;
+      const speed=data.wind.speed;
+      const gust=data.wind.gust;
+      const humidity=data.main.humidity;
+      const temp_max=data.main.temp_max;
+      const temp_min=data.main.temp_min;
+      const feels_like=data.main.feels_like;
+      const temp=data.main.temp;
+      // Create a new message
+      const messageResponse  = {
+        type: 'text',
+        text: `城市名稱:${city}\n
+               溫度:${temp}\n
+               體感溫度:${feels_like}\n
+               最低溫:${temp_min}\n
+               最高溫:${temp_max}\n
+               濕度:${humidity}\n
+               風速:${speed}\n
+               陣風:${gust}\n`,
+      };
+      await client.replyMessage(replyToken, messageResponse );
+    })
+    .catch((error) => {
+      // 处理请求过程中的错误
+      console.log(error);
+    });
   };
 
   try {
@@ -46,6 +82,7 @@ const handler = async (event: any) => {
     };
   } catch (error) {
     console.log(error);
+    console.log(event.body);
     return { statusCode: 500, body: error.toString() };
   }
 };
